9dd07c6248c395277315af8978636b87ffdd4cbd
k-Akeeranandan <rajiakeera@gmail.com>
2026-05-01 21:24:35 +0530

    changed the domain url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://project-backend-mo60.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
